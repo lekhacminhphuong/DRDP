28d71cf74263d7cd0609450606c01b7c3f8b9468
lekhacminhphuong <leanutella192@gmail.com>
2020-05-01 16:56:23 -0700

    Added report entry beginning page

---
+++ frontend/src/views/ReportEntryPage/data.js
@@ -0,0 +1,320 @@
+export default {
+    reportName: [
+      {
+        value: 'One PPR',
+        label: 'One PPR',
+      },
+      {
+        value: 'CENSUS',
+        label: 'CENSUS',
+      }
+    ],
+    reportYear: [
+      // {
+      //   value: '2010',
+      //   label: '2010',
+      // },
+      // {
+      //   value: '2011',
+      //   label: '2011',
+      // },
+      // {
+      //   value: '2012',
+      //   label: '2012',
+      // },
+      // {
+      //   value: '2013',
+      //   label: '2013',
+      // },
+      // {
+      //   value: '2014',
+      //   label: '2014',
+      // },
+      {
+        value: '2015',
+        label: '2015',
+      },
+      {
+        value: '2016',
+        label: '2016',
+      },
+      {
+        value: '2017',
+        label: '2017',
+      },
+      {
+        value: '2018',
+        label: '2018',
+      },
+      {
+        value: '2019',
+        label: '2019',
+      },
+      {
+        value: '2020',
+        label: '2020',
+      },
+      {
+        value: '2021',
+        label: '2021',
+      },
+      {
+        value: '2022',
+        label: '2022',
+      },
+      {
+        value: '2023',
+        label: '2023',
+      },
+      {
+        value: '2024',
+        label: '2024',
+      },
+      {
+        value: '2024',
+        label: '2024',
+      },
+      {
+        value: '2025',
+        label: '2025',
+      }
+    ],
+    reportState: [
+      {
+        value: 'Alabama',
+        label: 'Alabama',
+      },
+      {
+        value: 'Alaska',
+        label: 'Alaska',
+      },
+      {
+        value: 'American Samoa',
+        label: 'American Samoa',
+      },
+      {
+        value: 'Arizona',
+        label: 'Arizona',
+      },
+      {
+        value: 'Arkansas',
+        label: 'Arkansas',
+      },
+      {
+        value: 'California',
+        label: 'California',
+      },
+      {
+        value: 'Colorado',
+        label: 'Colorado',
+      },
+      {
+        value: 'Connecticut',
+        label: 'Connecticut',
+      },
+      {
+        value: 'Delaware',
+        label: 'Delaware',
+      },
+      {
+        value: 'District of Columbia',
+        label: 'District of Columbia',
+      },
+      {
+        value: 'Florida',
+        label: 'Florida',
+      },
+      {
+        value: 'Georgia',
+        label: 'Georgia',
+      },
+      {
+        value: 'Guam',
+        label: 'Guam',
+      },
+      {
+        value: 'Hawaii',
+        label: 'Hawaii',
+      },
+      {
+        value: 'Idaho',
+        label: 'Idaho',
+      },
+      {
+        value: 'Illinois',
+        label: 'Illinois',
+      },
+      {
+        value: 'Indiana',
+        label: 'Indiana',
+      },
+      {
+        value: 'Iowa',
+        label: 'Iowa',
+      },
+      {
+        value: 'Kansas',
+        label: 'Kansas',
+      },
+      {
+        value: 'Kentucky',
+        label: 'Kentucky',
+      },
+      {
+        value: 'Louisiana',
+        label: 'Louisiana',
+      },
+      {
+        value: 'Maine',
+        label: 'Maine',
+      },
+      {
+        value: 'Maryland',
+        label: 'Maryland',
+      },
+      {
+        value: 'Massachusetts',
+        label: 'Massachusetts',
+      },
+      {
+        value: 'Michigan',
+        label: 'Michigan',
+      },
+      {
+        value: 'Minnesota',
+        label: 'Minnesota',
+      },
+      {
+        value: 'Minor Outlying Islands',
+        label: 'Minor Outlying Islands',
+      },
+      {
+        value: 'Maine',
+        label: 'Maine',
+      },
+      {
+        value: 'Mississippi',
+        label: 'Mississippi',
+      },
+      {
+        value: 'Missouri',
+        label: 'Missouri',
+      },
+      {
+        value: 'Montana',
+        label: 'Montana',
+      },
+      {
+        value: 'Nebraska',
+        label: 'Nebraska',
+      },
+      {
+        value: 'Nevada',
+        label: 'Nevada',
+      },
+      {
+        value: 'New Hampshire',
+        label: 'New Hampshire',
+      },
+      {
+        value: 'New Jersey',
+        label: 'New Jersey',
+      },
+      {
+        value: 'New Mexico',
+        label: 'New Mexico',
+      },
+      {
+        value: 'New York',
+        label: 'New York',
+      },
+      {
+        value: 'North Carolina',
+        label: 'North Carolina',
+      },
+      {
+        value: 'North Dakota',
+        label: 'North Dakota',
+      },
+      {
+        value: 'Northern Mariana Islands',
+        label: 'Northern Mariana Islands',
+      },
+      {
+        value: 'Ohio',
+        label: 'Ohio',
+      },
+      {
+        value: 'Oklahoma',
+        label: 'Oklahoma',
+      },
+      {
+        value: 'Maine',
+        label: 'Maine',
+      },
+      {
+        value: 'Oregon',
+        label: 'Oregon',
+      },
+      {
+        value: 'Pennsylvania',
+        label: 'Pennsylvania',
+      },
+      {
+        value: 'Puerto Rico',
+        label: 'Puerto Rico',
+      },
+      {
+        value: 'Rhode Island',
+        label: 'Rhode Island',
+      },
+      {
+        value: 'South Carolina',
+        label: 'South Carolina',
+      },
+      {
+        value: 'South Dakota',
+        label: 'South Dakota',
+      },
+      {
+        value: 'Tennessee',
+        label: 'Tennessee',
+      },
+      {
+        value: 'Texas',
+        label: 'Texas',
+      },
+      {
+        value: 'U.S. Virgin Islands',
+        label: 'U.S. Virgin Islands',
+      },
+      {
+        value: 'Utah',
+        label: 'Utah',
+      },
+      {
+        value: 'Vermont',
+        label: 'Vermont',
+      },
+      {
+        value: 'Virginia',
+        label: 'Virginia',
+      },
+      {
+        value: 'Washington',
+        label: 'Washington',
+      },
+      {
+        value: 'West Virginia',
+        label: 'West Virginia',
+      },
+      {
+        value: 'Wisconsin',
+        label: 'Wisconsin',
+      },
+      {
+        value: 'Wyoming',
+        label: 'Wyoming',
+      }
+    ],
+  };
--- frontend/src/views/ReportEntryPage/index.js
+++ frontend/src/views/ReportEntryPage/index.js
@@ -2,30 +2,116 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Container } from '@material-ui/core';
 import Page from 'src/components/Page';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Typography, Button } from '@material-ui/core';
+import formData from './data';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
+    padding: theme.spacing(6),
+    paddingTop: theme.spacing(10)
   },
   header: {
-    marginBottom: theme.spacing(3),
+    marginBottom: theme.spacing(8),
   },
-  filter: {
-    marginTop: theme.spacing(3),
+  dropdown: {
+    width: "70%",
+    paddingBottom: theme.spacing(6),
+    paddingRight: theme.spacing(10)
   },
-  results: {
-    marginTop: theme.spacing(6),
-  },
+  button: {
+    paddingTop: theme.spacing(3)
+  }
 }));
 
 function ReportEntryPage() {
   const classes = useStyles();
+  const [reportName, setReportName] = React.useState('One PPR');
+  const [reportYear, setReportYear] = React.useState('2020');
+  const [reportState, setReportState] = React.useState('Alabama');
+
+  const handleReportName = (event) => {
+    setReportName(event.target.value);
+  };
+
+  const handleReportYear = (event) => {
+    setReportYear(event.target.value);
+  };
+
+  const handleReportState = (event) => {
+    setReportState(event.target.value);
+  };
 
   return (
     <Page className={classes.root} title="DRDP - Report Entry">
       <Container maxWidth="lg">
-        <p>This is Report Entry Page</p>
+        <div className={classes.header}>
+          <Typography
+            component="h1"
+            gutterBottom
+            variant="h3"
+          >
+            Report Entry
+          </Typography>
+          <Typography variant="h6">
+            Please select from the following options to Add/Modify a report
+          </Typography>
+        </div>
+
+        <TextField
+          select
+          className={classes.dropdown}
+          required
+          label="Report Name"
+          value={reportName}
+          onChange={handleReportName}
+          helperText="Please select report name"
+        >
+          {formData.reportName.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          select
+          className={classes.dropdown}
+          label="Report Year"
+          value={reportYear}
+          onChange={handleReportYear}
+          helperText="Please select report year"
+        >
+          {formData.reportYear.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          select
+          className={classes.dropdown}
+          label="Jurisdiction Name"
+          value={reportState}
+          onChange={handleReportState}
+          helperText="Please select jurisdiction name"
+        >
+          {formData.reportState.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <div className={classes.button}>
+          <Button
+            variant="outlined"
+            color="primary"
+            endIcon={<ArrowForwardIosIcon />}
+          >
+            Next
+        </Button>
+        </div>
       </Container>
     </Page>
   );
