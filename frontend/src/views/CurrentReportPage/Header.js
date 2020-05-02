5563d4a5cd263742f556efa693520d0a80039ed0
lekhacminhphuong <leanutella192@gmail.com>
2020-05-02 11:16:44 -0700

    Added Back button for data page

--- frontend/src/views/CurrentReportPage/Header.js
+++ frontend/src/views/CurrentReportPage/Header.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid, Button } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
-  dates: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end'
+  root: {
+    paddingBottom: theme.spacing(0.5)
   },
-  startDateButton: {
-    marginRight: theme.spacing(1)
-  },
-  endDateButton: {
-    marginLeft: theme.spacing(1)
-  },
-  calendarTodayIcon: {
-    marginRight: theme.spacing(1)
+  title: {
+    width: "85%"
   }
 }));
 
@@ -30,25 +22,43 @@ export default function Header({ className, ...rest }) {
       {...rest}
       className={clsx(classes.root, className)}
     >
-      <Typography
-        component="h2"
-        gutterBottom
-        variant="overline"
+      <Grid
+        container
+        justify="space-between"
       >
-        Gallery/CardName
-      </Typography>
-      <Typography
-        component="h1"
-        gutterBottom
-        variant="h3"
-      >
-        Card Name
-      </Typography>
-      <Typography variant="subtitle1">
-        This graph shows the total number of people served in the selected state for the selected years.
-        Select a state using the State filter.
-        Select years with the Year filter.
-      </Typography>
+        <Grid item className={classes.title}>
+          <Typography
+            component="h2"
+            gutterBottom
+            variant="overline"
+          >
+            Gallery/CardName
+          </Typography>
+          <Typography
+            component="h1"
+            gutterBottom
+            variant="h3"
+          >
+            Card Name
+          </Typography>
+          <Typography variant="subtitle1">
+            This graph shows the total number of people served in the selected state for the selected years.
+            Select a state using the State filter.
+            Select years with the Year filter.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            component={RouterLink}
+            variant="outlined"
+            color="primary"
+            endIcon={<ArrowBackIosIcon />}
+            to="/gallery"
+          >
+            Back
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
