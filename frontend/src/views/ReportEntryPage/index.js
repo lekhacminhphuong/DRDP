a24605a3cd7d01d74ce72c421c6e6ca0d12f170b
Joshua Canton <jtcanton@gmail.com>
2020-05-03 15:31:18 -0700

    report entry page

--- frontend/src/views/ReportEntryPage/index.js
+++ frontend/src/views/ReportEntryPage/index.js
@@ -25,7 +25,7 @@ function ReportEntryPage() {
   return (
     <Page className={classes.root} title="DRDP - Report Entry">
       <Container maxWidth="lg">
-        <p>This is Report Entry Page</p>
+        
       </Container>
     </Page>
   );
