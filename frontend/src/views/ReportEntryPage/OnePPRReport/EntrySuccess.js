bb018a46e9bee727e14a34249dbef754deebe3d7
lekhacminhphuong <leanutella192@gmail.com>
2020-05-21 12:04:33 -0700

    Added illustrator for report entry success

--- frontend/src/views/ReportEntryPage/OnePPRReport/EntrySuccess.js
+++ frontend/src/views/ReportEntryPage/OnePPRReport/EntrySuccess.js
@@ -2,54 +2,54 @@ import React, { useState, useContext } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Container, Typography } from '@material-ui/core';
 import Page from 'src/components/Page';
-import { Grid } from '@material-ui/core';
-import img from 'src/imgs/empty.svg'
+import img from 'src/imgs/success.svg'
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        paddingTop: theme.spacing(3),
-        paddingBottom: theme.spacing(3),
-    },
-    header: {
-        marginBottom: theme.spacing(3),
-    },
-    filter: {
-        marginTop: theme.spacing(3),
-    },
-    results: {
-        marginTop: theme.spacing(6),
-    },
-    empty: {
-        paddingTop: theme.spacing(10),
-        paddingLeft: theme.spacing(40)
-    },
-    noFav: {
-        paddingLeft: theme.spacing(15),
-    }
+  root: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+  header: {
+    marginBottom: theme.spacing(3),
+  },
+  filter: {
+    marginTop: theme.spacing(3),
+  },
+  results: {
+    marginTop: theme.spacing(6),
+  },
+  empty: {
+    paddingTop: theme.spacing(10),
+    paddingLeft: theme.spacing(33)
+  },
+  successMessage: {
+    paddingTop: theme.spacing(5),
+    paddingLeft: theme.spacing(25),
+  }
 }));
 
 
 function EntrySuccess() {
 
-    const classes = useStyles();
-    const [mode] = useState('grid');
+  const classes = useStyles();
+  const [mode] = useState('grid');
 
-    return (
-        <Page className={classes.root} title="DRDP - Entry Success">
-          <Container maxWidth="lg">
-            <div className={classes.empty}>
-              <img src={img} height="400" />
-              <Typography
-                className={classes.noFav}
-                gutterBottom
-                variant="subtitle1"
-              >
-                Report Submitted Successfully
+  return (
+    <Page className={classes.root} title="DRDP - Entry Success">
+      <Container maxWidth="lg">
+        <div className={classes.empty}>
+          <img src={img} height="400" />
+          <Typography
+            className={classes.successMessage}
+            gutterBottom
+            variant="subtitle1"
+          >
+            Report Submitted Successfully
              </Typography>
-            </div>
-          </Container>
-        </Page>
-      )
+        </div>
+      </Container>
+    </Page>
+  )
 }
 
 export default EntrySuccess;
